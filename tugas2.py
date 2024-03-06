f61a81ed1e3aa93adbbec02ef1743d5272328c88
gatshufitri <161842791+gatshufitri@users.noreply.github.com>
2024-03-06 17:10:20 +0700

    first commit

--- tugas2.py
+++ tugas2.py
@@ -1,16 +1,18 @@
 welcome = '''
-=================================
-=========Selamat Datang==========
-==========Di Cek Gaji============
-=================================
+===========================================================
+======================Selamat Datang=======================
+========================Di Cek Gaji========================
+===========================================================
 '''
 print (welcome)
-hari_kerja = 25
-hari_kerja_perbulan = 30 
-hari_lembur = 20
-gaji_pokok = 5000000
+print ("Silahkan Isi Data Dibawah Ini Untuk Menentukan Gaji Anda")
+nama = str (input ("Masukan Nama Anda : "))
+hari_kerja = int(input ("Masukan Hari Kerja Anda Dalam 1 Bulan : "))
+hari_kerja_perbulan = int (30)
+hari_lembur_perhari = int (input ("Masukan Hari Lembur Anda Dalam 1 Bulan : "))
+gaji_pokok = 2000000
 uang_lembur_perhari = 150000
+print ("Nama Karyawan",nama)
 
-total_gaji = (hari_kerja/hari_kerja_perbulan)*gaji_pokok+(hari_lembur*uang_lembur_perhari)
-total_gaji = int (total_gaji)
+total_gaji = int ((hari_kerja/hari_kerja_perbulan)*gaji_pokok+(hari_lembur_perhari*uang_lembur_perhari))
 print ("Gaji Anda Bulan Ini Rp. {:,}".format (total_gaji))
